36d8e005179717201d550ba26a56fc857ac6c5d6
l11040 <l1104033837@gmail.com>
2024-11-19 01:15:20 +0900

    [Update] RN 스플레시 제어

--- ios/seekgo/AppDelegate.mm
+++ ios/seekgo/AppDelegate.mm
@@ -1,19 +1,21 @@
 #import "AppDelegate.h"
 
-#import <React/RCTBundleURLProvider.h>
+#import <React/RCTBundleURLProvider.h> // React Native 번들 URL 제공자
+#import <RNSplashScreen.h>             // Splash Screen
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"seekgo";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
+  BOOL didFinishLaunchingWithOptions = [super application:application didFinishLaunchingWithOptions:launchOptions];
 
+  [RNSplashScreen show];
+
+  return didFinishLaunchingWithOptions;
+}
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
   return [self bundleURL];
