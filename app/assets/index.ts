64d7f0f1da76a1ceb711fe7603dbc7d6a8acd678
l11040 <l1104033837@gmail.com>
2024-11-20 22:53:39 +0900

    [Add] Person SVG 추가

--- app/assets/index.ts
+++ app/assets/index.ts
@@ -1,8 +1,13 @@
+import PersonIcon from './icons/person.svg';
+
 const Assets = {
   Images: {
     Logo: {
       title: require('./images/logo/logo-title.png'),
     },
+  },
+  Icons: {
+    Person: PersonIcon,
   },
 };
 
